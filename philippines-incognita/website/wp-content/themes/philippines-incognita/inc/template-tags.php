19e352948d1d988c0a7b2d6a22a9a317a805029e
“zascia” <zascia@ukr.net>
2021-05-19 07:19:16 +0300

    phils blog style of tags etc

--- philippines-incognita/website/wp-content/themes/philippines-incognita/inc/template-tags.php
+++ philippines-incognita/website/wp-content/themes/philippines-incognita/inc/template-tags.php
@@ -61,7 +61,7 @@ if (!function_exists('Philippines_Incognita_entry_footer')) :
             /* translators: used between list items, there is a space after the comma */
             $tags_list = get_the_tag_list('', esc_html__(', ', 'philippines-incognita'));
             if ($tags_list) {
-                printf(' | <span class="tags-links">' . esc_html__('Помечено %1$s', 'philippines-incognita') . '</span>', $tags_list); // WPCS: XSS OK.
+                printf(' | <span class="tags-links">' . esc_html__('Теги: %1$s', 'philippines-incognita') . '</span>', $tags_list); // WPCS: XSS OK.
             }
         }
 
