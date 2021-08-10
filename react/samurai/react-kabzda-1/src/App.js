17b2901bbc1aba58dc4be16e5670933407b55192
zascia <zascia@ukr.net>
2021-08-10 18:06:40 +0300

    kabzda new dialog component

--- react/samurai/react-kabzda-1/src/App.js
+++ react/samurai/react-kabzda-1/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import s from "./components/Dialogs/Dialogs.module.css";
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className='app-wrapper-content'>
+          <Profile />
+          {/*<Dialogs />*/}
+      </div>
     </div>);
 }
 
