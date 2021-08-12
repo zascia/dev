8a4db40ec3755ed097df4a99fdc9cb64084a9225
“zascia” <zascia@ukr.net>
2021-08-12 12:18:28 +0300

    kabzda adding links and router modification

--- react/samurai/react-kabzda-1/src/App.js
+++ react/samurai/react-kabzda-1/src/App.js
@@ -4,6 +4,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import NotFound from './components/NotFound/NotFound';
 import s from "./components/Dialogs/Dialogs.module.css";
 import {BrowserRouter, Route} from "react-router-dom";
 
@@ -16,6 +20,10 @@ const App = () => {
           <div className='app-wrapper-content'>
               <Route path='/dialogs' component={Dialogs} />
               <Route path='/profile' component={Profile} />
+              <Route path='/news' component={News} />
+              <Route path='/music' component={Music} />
+              <Route path='/settings' component={Settings} />
+              <Route component={NotFound} />
           </div>
         </div>
       </BrowserRouter>);
