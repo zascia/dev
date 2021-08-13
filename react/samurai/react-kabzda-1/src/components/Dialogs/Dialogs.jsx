bd2df866a320ec582bf93afd3ecea337002bf68b
zascia <zascia@ukr.net>
2021-08-13 18:30:42 +0300

    kabzda profile refactoring

--- react/samurai/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ react/samurai/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -2,33 +2,33 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path = "/dialogs/" + props.id;
+    return <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+}
+
+const Message = (props) => {
+    return <div className={s.message}>{props.message}</div>
+}
+
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to="/dialogs/1">Andrey</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/2">Sasha</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/3">Victor</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/4">Misha</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/5">Leonid</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/6">Leopard</NavLink>
-                </div>
+                <DialogItem name="Andrey" id="1" />
+                <DialogItem name="Sasha" id="2" />
+                <DialogItem name="Victor" id="3" />
+                <DialogItem name="Misha" id="4" />
+                <DialogItem name="Leonid" id="5" />
+                <DialogItem name="Leopard" id="6" />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>How are you</div>
-                <div className={s.message}>Yo</div>
+                <Message message="Hi" />
+                <Message message="How are you" />
+                <Message message="Yo" />
+                <Message message="Yo" />
             </div>
         </div>
     )
--- react/samurai/react-kabzda-1/src/components/Profile/Profile.jsx
+++ react/samurai/react-kabzda-1/src/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import MyPosts from "./MyPosts/MyPosts";
 import s from './Profile.module.css';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return <div>
-    <div>
-      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+        <ProfileInfo/>
+        <MyPosts/>
     </div>
-    <div>
-      ava + description
-    </div>
-    <MyPosts />
-  </div>
 }
 
 export default Profile;
