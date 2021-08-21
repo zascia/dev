7a2a25b2965ec751addfdee0285a85d09bf698ca
“zascia” <zascia@ukr.net>
2021-08-21 19:37:05 +0300

    move data to state.js and some refactoring

--- react/samurai/react-kabzda-1/src/index.js
+++ react/samurai/react-kabzda-1/src/index.js
@@ -3,30 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import state from './redux/state.js';
 
-let posts = [
-    {id:1,message:'Hi, how are you?', likesCount: 32},
-    {id:2,message:'It\'s my first post', likesCount: 132}
-]
-
-let dialogs = [
-    {id:1,name:'Andrey'},
-    {id:2,name:'Sasha'},
-    {id:3,name:'Victor'},
-    {id:4,name:'Misha'},
-    {id:5,name:'Leonid'},
-    {id:6,name:'Leopard'}
-]
-
-let messages = [
-    {id:1,message:'Hi'},
-    {id:2,message:'How are you'},
-    {id:3,message:'Yo'},
-    {id:4,message:'Yo'},
-    {id:5,message:'Yo'}
-]
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state={state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ react/samurai/react-kabzda-1/src/redux/state.js
@@ -0,0 +1,27 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id:1,message:'Hi, how are you?', likesCount: 32},
+            {id:2,message:'It\'s my first post', likesCount: 132}
+        ]
+    },
+    dialogsPage: {
+        messages: [
+            {id:1,message:'Hi'},
+            {id:2,message:'How are you'},
+            {id:3,message:'Yo'},
+            {id:4,message:'Yo'},
+            {id:5,message:'Yo'}
+        ],
+        dialogs: [
+            {id:1,name:'Andrey'},
+            {id:2,name:'Sasha'},
+            {id:3,name:'Victor'},
+            {id:4,name:'Misha'},
+            {id:5,name:'Leonid'},
+            {id:6,name:'Leopard'}
+        ]
+    }
+}
+
+export default state;
