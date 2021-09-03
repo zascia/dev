857711632cfc0a47625fd54938c38947f2396c86
zascia <zascia@ukr.net>
2021-09-03 10:36:44 +0300

    kabzda adding function to state and conecting it

--- react/samurai/react-kabzda-1/src/App.js
+++ react/samurai/react-kabzda-1/src/App.js
@@ -19,7 +19,7 @@ const App = (props) => {
           <Sidebar state={props.state.sidebarSection} />
           <div className='app-wrapper-content'>
               <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
-              <Route path='/profile' render={ () => <Profile state={props.state.profilePage} /> } />
+              <Route path='/profile' render={ () => <Profile state={props.state.profilePage} addPost={props.addPost} /> } />
               <Route path='/news' component={News} />
               <Route path='/music' component={Music} />
               <Route path='/settings' component={Settings} />
--- react/samurai/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ react/samurai/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -11,13 +11,30 @@ const Dialogs = (props) => {
     let messagesElements = props.state.messages
         .map( m => <Message message={m.message} />);
 
+    let newMessageElement = React.createRef();
+    let sendMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
+
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                { dialogsElements }
+        <div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    { dialogsElements }
+                </div>
+                <div className={s.messages}>
+                    { messagesElements }
+                </div>
             </div>
-            <div className={s.messages}>
-                { messagesElements }
+            <div>
+                <h3>Send message</h3>
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={sendMessage}>Send message</button>
+                </div>
             </div>
         </div>
     )
