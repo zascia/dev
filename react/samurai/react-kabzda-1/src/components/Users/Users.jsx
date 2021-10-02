3b0979595384c28653f471b6e7f0574dcf01814b
“zascia” <zascia@ukr.net>
2021-10-02 07:52:02 +0300

    kabzda user class refactoring --> adding didMount

--- react/samurai/react-kabzda-1/src/components/Users/Users.jsx
+++ react/samurai/react-kabzda-1/src/components/Users/Users.jsx
@@ -4,8 +4,8 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/boy-face-avatar.webp';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
