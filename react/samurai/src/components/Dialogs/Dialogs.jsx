8f3cedf336a6419b8ef3bc36afcc8fd571e6b9e0
“zascia” <zascia@ukr.net>
2021-10-15 18:46:20 +0300

    kabzda add redirect logic basic

--- react/samurai/src/components/Dialogs/Dialogs.jsx
+++ react/samurai/src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import {Redirect} from "react-router-dom";
 
 const Dialogs = (props) => {
 
@@ -22,6 +23,8 @@ const Dialogs = (props) => {
         let text = newMessageElement.current.value;
         props.updateNewMessageBody(text);
     };
+
+    if (!props.isAuth) return <Redirect to="/login" />;
 
     return (
         <div>
---
+++ react/samurai/src/components/Login/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = (props) => {
+    return <h1>Login page</h1>
+}
+
+export default Login;
