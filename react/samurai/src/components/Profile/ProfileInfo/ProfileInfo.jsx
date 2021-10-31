b11eea6a8adcaf990fe7b168410a6acde3c13792
“zascia” <zascia@ukr.net>
2021-10-31 07:04:58 +0200

    kabzda adding first hook --> useState

--- react/samurai/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ react/samurai/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../Common/Preloader/Preloader';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -15,7 +16,8 @@ const ProfileInfo = (props) => {
         </div>*/}
         <div className={s.descriptionBlock}>
             <img src={props.profile.photos.large} />
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+            {/*<ProfileStatus status={props.status} updateStatus={props.updateStatus} />*/}
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
         </div>
     </div>
 }
