3f81b1406df859377600f4cafe7bfe5f83b3bdc5
“zascia” <zascia@ukr.net>
2021-11-09 10:44:06 +0200

    kabzda more tetsing usecases

--- react/samurai/src/App.js
+++ react/samurai/src/App.js
@@ -10,10 +10,11 @@ import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
-import {connect} from 'react-redux';
+import {connect, Provider} from 'react-redux';
 import {compose} from 'redux';
 import {initializeApp} from './redux/app-reducer';
 import Preloader from './components/Common/Preloader/Preloader';
+import store from './redux/redux-store';
 
 class App extends React.Component {
     componentDidMount() {
@@ -47,7 +48,17 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-export default compose(
+let AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp})
 )(App);
+
+const SamuraiJSApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer />
+        </Provider>
+    </BrowserRouter>
+}
+
+export default SamuraiJSApp;
